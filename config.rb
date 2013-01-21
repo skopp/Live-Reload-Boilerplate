c694cb06c603c0ee94e195af04c0936dc1fee0c9
Jeremy Cloutier <jcloutier24@yahoo.com>
2013-01-21 13:59:16 -0500

    moved guardfile, gemfile and config.rb

--- config.rb
+++ config.rb
@@ -1,12 +1,12 @@
 # Require any additional compass plugins here.
 
 # Set this to the root of your project when deployed:
-http_path = "/assets"
-css_dir = "css"
-sass_dir = "src/sass"
-images_dir = "img"
-javascripts_dir = "js"
-fonts_dir = "fonts"
+http_path = "/"
+css_dir = "assets/css"
+sass_dir = "assets/src/sass"
+images_dir = "assets/img"
+javascripts_dir = "assets/js"
+fonts_dir = "assets/fonts"
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
